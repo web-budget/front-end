751eafd635a00a6b28bb00d77505f3f20088fe61
Arthur Gregório <arthurshakal@gmail.com>
2022-09-13 23:43:30 -0300

    ref #25

--- src/views/registration/cost-center/CostCenterFormView.vue
+++ src/views/registration/cost-center/CostCenterFormView.vue
@@ -1,27 +1,25 @@
 <template>
   <page-content pre-title="cost-center.header.pre-title" title="cost-center.header.title">
-    <form>
+    <Form @submit="doSave" :validation-schema="schema" :initial-values="formDefaults" v-slot="{ errors }">
       <div class="col-12">
         <div class="card">
           <div class="card-header">
             <div class="btn-group">
-              <input
+              <Field
                 id="rdAll"
                 type="radio"
-                name="rdStatus"
                 value="false"
                 class="btn-check"
-                v-model="formData.active">
+                name="active" />
               <label for="rdAll" class="btn btn-outline-danger">
                 {{ $t('form.status.inactive') }}
               </label>
-              <input
+              <Field
                 id="rdActive"
                 type="radio"
-                name="rdStatus"
                 value="true"
                 class="btn-check"
-                v-model="formData.active">
+                name="active" />
               <label for="rdActive" class="btn btn-outline-success">
                 {{ $t('form.status.active') }}
               </label>
@@ -31,11 +29,12 @@
             <div class="row">
               <div class="col-12 mb-3">
                 <label class="form-label required">{{ $t('cost-center.form.name') }}</label>
-                <input type="text" class="form-control" v-model="formData.name" autocomplete="off"/>
+                <Field type="text" name="name" class="form-control" :class="{ 'is-invalid': errors.name }" autocomplete="off"/>
+                <div class="invalid-feedback">{{ errors.name }}</div>
               </div>
               <div class="col-12">
                 <label class="form-label">{{ $t('cost-center.form.description') }}</label>
-                <textarea class="form-control" rows="4" v-model="formData.description" data-bs-toggle="autosize" />
+                <Field as="textarea" name="description" class="form-control" rows="4" data-bs-toggle="autosize" />
               </div>
             </div>
           </div>
@@ -43,13 +42,13 @@
             <div class="row">
               <div class="col text-end">
                 <button class="btn btn-ghost-secondary me-3" @click.prevent="goBack()">{{ $t('form.actions.back') }}</button>
-                <button class="btn btn-primary" @click.prevent="save()">{{ $t('form.actions.save') }}</button>
+                <button type="submit" class="btn btn-primary">{{ $t('form.actions.save') }}</button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </form>
+    </Form>
   </page-content>
 </template>
 
@@ -58,30 +57,39 @@ import { reactive } from 'vue'
 
 import router from '@/router'
 
+import * as Yup from 'yup'
+
+import { Form, Field } from 'vee-validate'
+
 // import { useHttpErrorHandler } from '@/composables/useHttpErrorHandler.js'
-import { useMessageHandler } from '@/composables/useMessageHandler.js'
+// import { useMessageHandler } from '@/composables/useMessageHandler.js'
 
 import PageContent from '@/components/home/PageContent.vue'
 
-const { displaySuccess } = useMessageHandler()
+// const { displaySuccess } = useMessageHandler()
 // const { handleError } = useHttpErrorHandler()
 
-const formData = reactive({
-  active: true,
+const formDefaults = reactive({
+  active: 'true',
   name: null,
   description: null
 })
 
-function save() {
-  try {
-    // loading true
-    console.log(formData)
-    displaySuccess('form.messages.saved')
-  } catch (error) {
-    // handleError(error.response)
-  } finally {
-    // loading false
-  }
+const schema = Yup.object().shape({
+  name: Yup.string().min(3).max(150).required()
+})
+
+function doSave(values) {
+  console.log(values)
+  // try {
+  //   // loading true
+  //   console.log(formData)
+  //   displaySuccess('form.messages.saved')
+  // } catch (error) {
+  //   // handleError(error.response)
+  // } finally {
+  //   // loading false
+  // }
 }
 
 function goBack() {
