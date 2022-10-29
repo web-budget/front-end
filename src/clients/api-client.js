bc3e1c2e6dc245cb39bc0b7aa48ee5eef1ff0a3e
Arthur Gregório <arthurshakal@gmail.com>
2022-10-29 18:47:16 -0300

    fix request logger param

--- src/clients/api-client.js
+++ src/clients/api-client.js
@@ -34,7 +34,7 @@ class ApiClient {
       this.client.interceptors.request.use(authInterceptor)
     }
 
-    const debugEnabled = process.env.VUE_APP_LOG_REQUESTS || false
+    const debugEnabled = process.env.VUE_APP_LOG_REQUESTS === 'true'
     if (debugEnabled) {
       this.client.interceptors.request.use(loggingInterceptor)
     }
