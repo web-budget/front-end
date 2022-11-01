a5a8525b3e7d56ec11a6eff8d4e29a9efded3e66
Arthur Gregório <arthurshakal@gmail.com>
2022-10-31 22:04:02 -0300

    fix #38

--- src/clients/api-client.js
+++ src/clients/api-client.js
@@ -24,7 +24,7 @@ const loggingInterceptor = config => {
   const requestData = {
     method: config.method.toUpperCase(),
     url: `${config.baseURL}${config.url}`,
-    params: config.params || null
+    ...config.params
   }
   console.info(requestData)
   return config
