c97b2e5b6f4b9cdbc5f7d501439f430f2b5ccc49
Arthur Gregorio <arthurshakal@gmail.com>
2023-04-16 00:32:28 -0300

    fix #53

--- src/components/templates/LoginTemplate.vue
+++ src/components/templates/LoginTemplate.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="page page-center">
-    <div class="container container-tight py-4">
-      <div class="text-center mb-4">
-        <span class="wb-logo">web</span>
-        <span class="wb-logo wb-logo-black">Budget</span>
-      </div>
-      <router-view />
+  <div class="container container-tight py-4">
+    <div class="text-center mb-4">
+      <span class="wb-logo">web</span>
+      <span class="wb-logo wb-logo-black">Budget</span>
     </div>
+    <router-view />
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.body.classList.add('d-flex', 'flex-column')
+  document
+    .querySelector('#app')
+    .classList.add('page', 'page-center', 'adjust-logo')
+})
+</script>
+
+<style lang="scss">
+.adjust-logo {
+  top: -120px;
+}
+</style>
