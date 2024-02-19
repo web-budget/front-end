da513e1e404a032aefeb86368f43df280e1fcaca
Arthur Gregorio <arthurshakal@gmail.com>
2024-02-18 22:24:51 -0300

    update configs

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 export default defineConfig({
   plugins: [vue(), VueI18nPlugin()],
   build: {
-    cssCodeSplit: true,
+    cssCodeSplit: false,
   },
   server: {
     port: 8080,
