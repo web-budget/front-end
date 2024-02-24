88dfc9d81946bc0821b17af5af2ec07f145542cd
Arthur Gregorio <arthurshakal@gmail.com>
2024-02-24 17:11:24 -0300

    remove code split config

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
   plugins: [vue(), VueI18nPlugin()],
-  build: {
-    cssCodeSplit: false,
-  },
   server: {
     port: 8080,
   },
