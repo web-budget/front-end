6ba95f00fe2d139e8436001d1ab3503c48fca0f8
Arthur Gregorio <arthurshakal@gmail.com>
2026-01-04 23:30:04 -0300

    ref #95: adding config

---
+++ vite.config.mjs
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    noDiscovery: true,
+    include: ['yup']
+  },
+  server: {
+    port: 8080
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      resolvers: [PrimeVueResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
