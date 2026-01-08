ae71c1abdde314f931dbe469c22502a6f8faf563
Arthur Gregorio <arthurshakal@gmail.com>
2026-01-08 00:00:22 -0300

    fix #95: adjusting linter

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,17 @@ import pluginVue from 'eslint-plugin-vue'
 import eslintConfigPrettier from '@vue/eslint-config-prettier'
 
 export default [
-    {
-        name: 'app/files-to-lint',
-        files: ['**/*.{js,mjs,jsx,vue}']
-    },
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}']
+  },
 
-    {
-        name: 'app/files-to-ignore',
-        ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
-    },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
 
-    js.configs.recommended,
-    ...pluginVue.configs['flat/essential'],
-    eslintConfigPrettier
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  eslintConfigPrettier
 ]
